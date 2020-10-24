6aff6ca578ba78454ccb3b0ebb57663a4cacc128
HPEONIX <arjunpramod1@gmail.com>
2020-10-24 14:47:55 +0530

    initial
    created player movement

---
+++ Assets/Scripts/Cantroller/PlayerCantroller.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using movement;
+using UnityEngine;
+
+namespace Cantrollers
+{ 
+    [RequireComponent(typeof(Move))]
+    public class PlayerCantroller : MonoBehaviour
+    {
+
+        public bool isDed = false;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isDed)
+                return;
+            if (respondAction())
+                return;
+            if (movementAction())
+                return;
+        
+        }
+
+        //todo : interact with interactable
+        private bool respondAction()
+        {
+            return false;
+        }
+
+        private bool movementAction()
+        {
+            Vector2 screenpos = default;
+            screenpos = GetScreenPos();
+            if (screenpos == null || screenpos == default) return false;
+            return movetoWorldPoint(screenpos);
+
+
+        }
+
+        private static Vector2 GetScreenPos()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                return Input.mousePosition;
+            }
+            if (Input.touchCount > 0)
+            {
+                return Input.GetTouch(0).position;
+            }
+            return default;
+        }
+
+        private bool movetoWorldPoint(Vector2 screenpos)
+        {
+            var touch_ray = Camera.main.ScreenPointToRay(screenpos);
+            RaycastHit hit;
+            if (Physics.Raycast(touch_ray,out hit))
+            {
+                GetComponent<Move>().moveTo(hit.point);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/Mover/Move.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace movement
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class Move : MonoBehaviour
+    {
+        public float maxSpeed=6f;
+
+        [Range(0,1)]
+        public float speedCantrol=1f;
+
+        NavMeshAgent meshAgent;
+
+        private void Awake()
+        {
+            meshAgent = GetComponent<NavMeshAgent>();
+        }
+        public void moveTo(Vector3 position)
+        {
+            meshAgent.isStopped = false;
+            setSpeed(speedCantrol);
+            meshAgent.SetDestination(position);
+        }
+        public void pauseAction()
+        {
+            meshAgent.isStopped = true;
+        }
+
+        public void stopAction()
+        {
+            pauseAction();
+            meshAgent.SetDestination(transform.position);
+        }
+
+        public void setSpeed(float speedCantrol)
+        {
+            meshAgent.speed = maxSpeed * Mathf.Clamp01( speedCantrol);
+        }
+
+
+    }
+
+}
