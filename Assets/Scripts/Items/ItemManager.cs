6a62f23337ec3a01ad3fae82a7de2df0c12db7bd
HPEONIX <arjunpramod1@gmail.com>
2020-10-25 21:10:03 +0530

    added item and door
    collect all item to open door

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -6,10 +6,25 @@ public class ItemManager : MonoBehaviour
 {
     public GameObject wayBlocker;
 
-    int count =>transform.childCount;
+    int count ;
     // Start is called before the first frame update
+    private void Start()
+    {
+        count = transform.childCount;
+    }
     bool check()
     {
         return count == 0;
+    }
+    
+    public void openDoor()
+    {
+        count -= 1;
+        if (wayBlocker == null) return;
+        if(check())
+        {
+            wayBlocker.SetActive(false);
+            return;
+        }
     }
 }
--- Assets/Scripts/Items/ItemPickup.cs
+++ Assets/Scripts/Items/ItemPickup.cs
@@ -7,9 +7,11 @@ public class ItemPickup : MonoBehaviour
     public GameObject particle;
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("player"))
+        if(other.CompareTag("Player"))
         {
             Instantiate(particle, transform.position, transform.rotation);
+            var tmp = transform.parent.GetComponent<ItemManager>();
+            if (tmp != null) tmp.openDoor();
             Destroy(gameObject);
         }
     }
