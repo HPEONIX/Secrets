a4312e7e1bf0bbd82d41bf61226754de27a43b40
HPEONIX <arjunpramod1@gmail.com>
2020-10-25 20:51:24 +0530

    fix
    fix

---
+++ Assets/Scripts/Items/ItemManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    public GameObject wayBlocker;
+
+    int count =>transform.childCount;
+    // Start is called before the first frame update
+    bool check()
+    {
+        return count == 0;
+    }
+}
